92ee1b739cb8bdb259d6265966b4ed8c774b7c17
5Prabesh <pbakhrel5@gmail.com>
2024-07-13 20:58:36 +0545

    Login and signup screen updated

--- src/Screens/AuthScreens/LoginScreen.js
+++ src/Screens/AuthScreens/LoginScreen.js
@@ -1,37 +1,20 @@
 import { View, Text, ImageBackground, StyleSheet, Dimensions, TouchableOpacity, TextInput, ScrollView, KeyboardAvoidingView, StatusBar } from 'react-native';
 import React from 'react';
-import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import InputField from '../../Components/InputField';
 import CustomButton from '../../Components/CustomButton';
+import ImageContainer from '../../Components/ImageContainer';
 
 const LoginScreen = ({ navigation }) => {
   const { width, height } = Dimensions.get('window');
 
   const styles = StyleSheet.create({
-    container: { flex: 1 },
-    imageStyle: {
-      height: height * 0.4,
-      width: width,
-    },
-    backIcon: {
-      fontSize: 20,
-      fontWeight: 'bold',
-      color: 'black',
-    },
-    backText: {
-      color: 'black',
-      fontSize: 15,
-      marginLeft: 4,
-      fontWeight: 'bold',
-    },
-    backButtonContainer: {
-      flexDirection: 'row',
-      margin: 10
+    container: {
+      flex: 1
     },
     loginTextContainer: {
       margin: 15
     },
-    loginText: {
+    loginText: {  
       color: 'black',
       fontSize: 25,
       fontWeight: 'bold',
@@ -44,37 +27,24 @@ const LoginScreen = ({ navigation }) => {
       color: 'black',
       fontWeight: 'bold',
     },
-    footerContainer:{
-      flexDirection:'row',
-      alignItems:'center',
-      marginTop: 15,
-      justifyContent:'center'
+    footerContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginTop: 20,
+      justifyContent: 'center'
     },
     footerText: {
-      color:'black',
-      fontWeight:'500'
+      color: 'black',
+      fontWeight: '500'
     }
   })
 
   return (
     <View style={styles.container}>
-      <StatusBar backgroundColor={'white'} barStyle={'dark-content'}/>
-      <View>
-        <ImageBackground source={require('../../assets/images/loginImage.png')} style={styles.imageStyle}>
+      <StatusBar backgroundColor={'white'} barStyle={'dark-content'} />
 
-          <TouchableOpacity
-            style={styles.backButtonContainer}
-            activeOpacity={0.4}
-            onPress={() => navigation.goBack()}
-          >
-            <FontAwesome5 name={'chevron-left'} style={styles.backIcon} />
-            <Text style={styles.backText}>
-              Back
-            </Text>
-          </TouchableOpacity>
+      <ImageContainer path={require('../../assets/images/loginImage.png')} onPress={()=>navigation.goBack()}/>
 
-        </ImageBackground>
-      </View>
       <ScrollView>
         <View style={styles.loginTextContainer}>
           <Text style={styles.loginText}>
@@ -92,18 +62,18 @@ const LoginScreen = ({ navigation }) => {
             </Text>
           </TouchableOpacity>
         </View>
-        <View style={{marginTop: 15}}>
+        <View style={{ marginTop: 10 }}>
           <CustomButton label={'Login'} showGoogleIcon={false} />
-          <Text style={{ textAlign: 'center', margin: 5, fontWeight: 'bold', color: 'black' }}>OR</Text>
-          <CustomButton label={'Continue with Google'} showGoogleIcon={true} backgroundColor={'#D9D9D9'} textColor={'black'}/>
+          <Text style={{ textAlign: 'center', margin: 4, fontWeight: 'bold', color: 'black' }}>OR</Text>
+          <CustomButton label={'Continue with Google'} showGoogleIcon={true} backgroundColor={'#D9D9D9'} textColor={'black'} />
         </View>
         <View style={styles.footerContainer}>
           <Text style={styles.footerText}>
-            Don't have an account? 
+            Don't have an account?
           </Text>
-          <TouchableOpacity 
-          onPress={()=>{navigation.navigate('SignUp')}}
-          activeOpacity={0.7}
+          <TouchableOpacity
+            onPress={() => { navigation.navigate('SignUp') }}
+            activeOpacity={0.7}
           >
             <Text>
               {'\t'}SignUp
--- src/Screens/AuthScreens/SignUpScreen.js
+++ src/Screens/AuthScreens/SignUpScreen.js
@@ -1,10 +1,47 @@
-import { View, Text } from 'react-native'
+import { View, Text, StatusBar, Dimensions, StyleSheet, TouchableOpacity, ScrollView } from 'react-native'
 import React from 'react'
+import ImageContainer from '../../Components/ImageContainer';
+import InputField from '../../Components/InputField';
+import CustomButton from '../../Components/CustomButton';
 
-const SignUpScreen = () => {
+const SignUpScreen = ({ navigation }) => {
+  const { width, height } = Dimensions.get('screen');
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1
+    },
+    signUpTextContainer: {
+      margin: 15
+    },
+    loginText: {
+      color: 'black',
+      fontSize: 25,
+      fontWeight: 'bold',
+    },
+  })
   return (
-    <View>
-      <Text>SignUpScreen</Text>
+    <View style={styles.container}>
+      <StatusBar backgroundColor={'white'} barStyle={'dark-content'} />
+      <ImageContainer path={require('../../assets/images/signUpImage.png')} onPress={() => navigation.goBack()} />
+      <ScrollView>
+        <View style={styles.signUpTextContainer}>
+          <Text style={styles.loginText}>
+            Sign up
+          </Text>
+          <InputField label={'Enter email address'} />
+          <InputField label={'Enter password'} type={'password'} />
+          <InputField label={'Confirm password'} type={'password'} />
+
+        </View>
+        <View style={{marginTop: 15,alignItems:'center'}}>
+          <Text>
+            By signing up, you are agree to our <Text style={{color:'black', fontWeight:'bold'}}>Terms, Conditions{'\n'}</Text> and <Text style={{color:'black', fontWeight:'bold'}}>Policies.</Text>
+          </Text>
+        </View>
+        <View style={{marginTop: 30}}>
+        <CustomButton label={'Continue'} onPress={()=>navigation.navigate('Login')}/>
+        </View>
+      </ScrollView>
     </View>
   )
 }
