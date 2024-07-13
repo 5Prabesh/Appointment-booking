5564eeb174113515fdeab4196622c8fa445b4312
5Prabesh <pbakhrel5@gmail.com>
2024-07-13 17:54:14 +0545

    SignUpScreen created and added in authStack

--- src/Navigation/AuthStack.js
+++ src/Navigation/AuthStack.js
@@ -1,6 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import OnBoardingScreen from "../Screens/AuthScreens/OnBoardingScreen";
 import LoginScreen from "../Screens/AuthScreens/LoginScreen";
+import SignUpScreen from "../Screens/AuthScreens/SignUpScreen";
 
 const AuthStack = () => {
     const Stack = createNativeStackNavigator();
@@ -13,6 +14,7 @@ const AuthStack = () => {
         >
             <Stack.Screen name = 'OnBoarding' component={OnBoardingScreen} />
             <Stack.Screen name = 'Login' component={LoginScreen} />
+            <Stack.Screen name = 'SignUp' component={SignUpScreen} />
         </Stack.Navigator>
     )
 }
