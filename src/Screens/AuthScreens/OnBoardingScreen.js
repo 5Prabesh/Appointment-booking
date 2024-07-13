7f4989f6fc87483b12f6c9ca2e99f42ab2b3b40e
5Prabesh <pbakhrel5@gmail.com>
2024-07-13 17:53:25 +0545

    Custom Button implemented in OnBoardingScreen

--- src/Screens/AuthScreens/OnBoardingScreen.js
+++ src/Screens/AuthScreens/OnBoardingScreen.js
@@ -1,6 +1,7 @@
 import { View, Text, Image, ScrollView, StyleSheet, StatusBar, Dimensions, TouchableOpacity } from 'react-native';
 import React from 'react';
 import ImageSlider from '../../Components/slider';
+import CustomButton from '../../Components/CustomButton';
 
 const OnBoardingScreen = ({ navigation }) => {
   const images = [
@@ -12,7 +13,7 @@ const OnBoardingScreen = ({ navigation }) => {
     require('../../assets/images/onBoarding3.png')
 
   ];
-  const {width} = Dimensions.get('window')
+  const { width } = Dimensions.get('window')
 
   const styles = StyleSheet.create({
     container: {
@@ -20,43 +21,18 @@ const OnBoardingScreen = ({ navigation }) => {
       backgroundColor: 'white',
       paddingTop: StatusBar.currentHeight
     },
-    buttonView :{
-      marginTop: 70,
-      alignItems:'center'
-    },
-    button : {
-      backgroundColor: 'black',
-      width: width * 0.8,
-      height: 50,
-      borderRadius: 20,
-      alignItems:'center',
-      justifyContent:'center'
-    },
-    buttonText : {
-      textAlign:'center',
-      fontSize: 16,
-      fontWeight:'bold',
-      color:'white',
-
-    }
   })
-
-
   return (
     <View style={styles.container}>
       <StatusBar backgroundColor={'white'} barStyle={'dark-content'} />
       <ImageSlider images={images} navigation={navigation} />
-      <View style={styles.buttonView}>
-        <TouchableOpacity 
-        style={styles.button}
-        activeOpacity={0.9}
+      <View style={{ marginTop: 50 }}>
+        <CustomButton 
+        label={"Let's Go"} 
         onPress={()=>navigation.navigate('Login')}
-        >
-          <Text style={styles.buttonText}>
-            Let's Go
-          </Text>
-        </TouchableOpacity>
+        />
       </View>
+
     </View>
   )
 }
