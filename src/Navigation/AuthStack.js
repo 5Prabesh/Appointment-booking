a11d26458cc98fe3f0eae69694b0a9387ca9866c
5Prabesh <pbakhrel5@gmail.com>
2024-07-14 09:49:46 +0545

    Otp screen made and added in auth stack

--- src/Navigation/AuthStack.js
+++ src/Navigation/AuthStack.js
@@ -3,6 +3,7 @@ import OnBoardingScreen from "../Screens/AuthScreens/OnBoardingScreen";
 import LoginScreen from "../Screens/AuthScreens/LoginScreen";
 import SignUpScreen from "../Screens/AuthScreens/SignUpScreen";
 import ForgotPasswordScreen from "../Screens/AuthScreens/ForgotPasswordScreen";
+import OtpScreen from "../Screens/AuthScreens/OtpScreen";
 
 const AuthStack = () => {
     const Stack = createNativeStackNavigator();
@@ -17,6 +18,7 @@ const AuthStack = () => {
             <Stack.Screen name = 'Login' component={LoginScreen} />
             <Stack.Screen name = 'SignUp' component={SignUpScreen} />
             <Stack.Screen name = 'ForgotPassword' component={ForgotPasswordScreen} />
+            <Stack.Screen name = 'Otp' component={OtpScreen} />
         </Stack.Navigator>
     )
 }
