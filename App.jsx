7217447ef7365e776bda375f6aad8a23ab7457dc
5Prabesh <pbakhrel5@gmail.com>
2024-07-21 20:50:08 +0545

    conditional rendering implemented

--- App.jsx
+++ App.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AuthStack from './src/Navigation/AuthStack';
+import Tabs from './src/Navigation/Tabs';
+import auth from '@react-native-firebase/auth';
+
 
 const App = () => {
+  const [user, setUser] = React.useState(null);
+
+  React.useEffect(() => {
+    const unsubscribe = auth().onAuthStateChanged(user => {
+      setUser(user);
+    });
+
+    return unsubscribe; // Cleanup subscription on unmount
+  }, []);
+
   return (
-      <NavigationContainer>
-        <AuthStack />
-      </NavigationContainer>
-  )
-}
+    <NavigationContainer>
+      {user ? <Tabs/> : <AuthStack/>}
+    </NavigationContainer>
+  );
+};
 
 export default App;
