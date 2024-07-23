ae99277d63c5d4a4c86d864d70781878b016cf10
5Prabesh <pbakhrel5@gmail.com>
2024-07-23 22:07:39 +0545

    home screen updated

--- src/Screens/TabScreens/HomeScreen.js
+++ src/Screens/TabScreens/HomeScreen.js
@@ -1,9 +1,11 @@
-import { View, Text, Image, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
+import { View, Text, Image, StyleSheet, TouchableOpacity, ScrollView, StatusBar } from 'react-native';
 import React from 'react';
 import SearchField from '../../Components/SearchField';
+import DoctorCard from '../../Components/Card';
 
 
 const HomeScreen = () => {
+  
   const styles = StyleSheet.create({
     userProfileImage: {
       height: 45,
@@ -35,31 +37,38 @@ const HomeScreen = () => {
       color: 'black',
     },
     image: {
-      width: 100,
-      height: 100,
+      width: 90,
+      height: 90,
       borderRadius: 10
+    },
+    popularTextContainer:{
+      margin: 20
+    },
+    popularText:{
+      fontSize: 20,
+      fontWeight: '600',
+      color: 'black',
     }
   })
   return (
-    <View style={{ flex: 1 }}>
+    <ScrollView style={{ flex: 1, backgroundColor:'white'}} showsVerticalScrollIndicator={false}>
+      <StatusBar backgroundColor={'white'} barStyle={'dark-content'}/>
       <View style={styles.profileContainer}>
-        <TouchableOpacity>
+        <TouchableOpacity activeOpacity={0.6}>
           <Image source={require('../../assets/icons/user.png')} style={styles.userProfileImage} />
         </TouchableOpacity>
         <View style={styles.usernameContainer}>
           <Text style={styles.usernameText}>User Name</Text>
         </View>
       </View>
-
+      
       {/* searchbar */}
       <SearchField iconName={'search'} label={'Search'} />
-
-      <ScrollView>
         <View style={styles.headContainer}>
           <Text style={styles.headText}>
             Looking For
           </Text>
-          <TouchableOpacity>
+          <TouchableOpacity activeOpacity={0.6}>
             <Text style={{ fontWeight: 'bold', fontSize: 15 }}>
               More
             </Text>
@@ -73,32 +82,38 @@ const HomeScreen = () => {
         marginLeft:20,
         }}>
           <View>
-            <TouchableOpacity>
+            <TouchableOpacity activeOpacity={0.7}>
               <Image source={require('../../assets/images/DoctorImage.png')} style={styles.image} />
               <Text>Doctor</Text>
             </TouchableOpacity>
           </View>
           <View style={{marginLeft:15}}>
-            <TouchableOpacity>
+            <TouchableOpacity activeOpacity={0.7}>
               <Image source={require('../../assets/images/DentistImage.png')} style={styles.image} />
               <Text>Dentist</Text>
             </TouchableOpacity>
           </View>
           <View style={{marginLeft:15}}>
-            <TouchableOpacity>
+            <TouchableOpacity activeOpacity={0.7}>
               <Image source={require('../../assets/images/HairDresserImage.png')} style={styles.image} />
               <Text>Hair Dresser</Text>
             </TouchableOpacity>
           </View>
           <View style={{marginLeft:15, marginRight: 15}}>
-            <TouchableOpacity>
+            <TouchableOpacity activeOpacity={0.7}>
               <Image source={require('../../assets/images/Trainer.png')} style={styles.image} />
               <Text>Trainer</Text>
             </TouchableOpacity>
           </View>
         </ScrollView>
+        <View style={styles.popularTextContainer}>
+          <Text style={styles.popularText}>
+            Popular
+          </Text>
+        </View>
+        {/* card */}
+        <DoctorCard />
       </ScrollView>
-    </View>
   );
 };
 
