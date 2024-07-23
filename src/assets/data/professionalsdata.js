fb252a0ff51d572015f78c8e7b42327098cf7635
5Prabesh <pbakhrel5@gmail.com>
2024-07-23 22:05:51 +0545

    Mock data added

---
+++ src/assets/data/professionalsdata.js
@@ -0,0 +1,64 @@
+const professionalsData = [
+    {
+        id: 1,
+        image: "https://t4.ftcdn.net/jpg/02/60/04/09/360_F_260040900_oO6YW1sHTnKxby4GcjCvtypUCWjnQRg5.jpg",
+        name: "Dr. Andrew",
+        description:'Dr. Andrew is an experienced dentist with over 10 years of practice. He specializes in general dentistry and offers a range of services.',
+        profession: "Doctor",
+        rating: 4.5,
+        reviews: 128,
+        isPopular: 'true'
+    },
+    {
+        id: 2,
+        image: "https://t4.ftcdn.net/jpg/02/60/04/09/360_F_260040900_oO6YW1sHTnKxby4GcjCvtypUCWjnQRg5.jpg",
+        name: "Dr. Prabesh",
+        description:'Dr. Prabesh is an experienced dentist with over 10 years of practice. He specializes in general dentistry and offers a range of services.',
+        profession: "Dentist",
+        rating: 4.5,
+        reviews: 128,
+        isPopular: 'true'
+    },
+    {
+        id: 3,
+        image: "https://t4.ftcdn.net/jpg/02/60/04/09/360_F_260040900_oO6YW1sHTnKxby4GcjCvtypUCWjnQRg5.jpg",
+        name: "Dr. Ram",
+        description:'Dr. Ram is an experienced dentist with over 10 years of practice. He specializes in general dentistry and offers a range of services.',
+        profession: "Trainer",
+        rating: 4.5,
+        reviews: 128,
+        isPopular: 'false'
+    },
+    {
+        id: 4,
+        image: "https://t4.ftcdn.net/jpg/02/60/04/09/360_F_260040900_oO6YW1sHTnKxby4GcjCvtypUCWjnQRg5.jpg",
+        name: "Dr. Andrew",
+        description:'Dr. Andrew is an experienced dentist with over 10 years of practice. He specializes in general dentistry and offers a range of services.',
+        profession: "Doctor",
+        rating: 4.5,
+        reviews: 128,
+        isPopular: 'true'
+    },
+    {
+        id: 5,
+        image: "https://t4.ftcdn.net/jpg/02/60/04/09/360_F_260040900_oO6YW1sHTnKxby4GcjCvtypUCWjnQRg5.jpg",
+        name: "Dr. Prabesh",
+        description:'Dr. Prabesh is an experienced dentist with over 10 years of practice. He specializes in general dentistry and offers a range of services.',
+        profession: "Dentist",
+        rating: 4.5,
+        reviews: 128,
+        isPopular: 'true'
+    },
+    {
+        id: 6,
+        image: "https://t4.ftcdn.net/jpg/02/60/04/09/360_F_260040900_oO6YW1sHTnKxby4GcjCvtypUCWjnQRg5.jpg",
+        name: "Dr. Ram",
+        description:'Dr. Ram is an experienced dentist with over 10 years of practice. He specializes in general dentistry and offers a range of services.',
+        profession: "Trainer",
+        rating: 4.5,
+        reviews: 128,
+        isPopular: 'false'
+    },
+];
+
+export default professionalsData;
