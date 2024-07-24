a32b1b7e179b5f9b0e3b765d371d7cc47e9f5ffb
5Prabesh <pbakhrel5@gmail.com>
2024-07-24 22:25:51 +0545

    .

--- src/Screens/TabScreens/ProfileScreen.js
+++ src/Screens/TabScreens/ProfileScreen.js
@@ -1,10 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, Text,StyleSheet } from 'react-native'
 import React from 'react'
 
 const ProfileScreen = () => {
+  const styles = StyleSheet.create({
+    profileContainer:{
+      alignItems:'center'
+    }
+  })
+
+
   return (
-    <View>
-      <Text>ProfileScreen</Text>
+    <View style={{flex:1}}>
+      <View style={styles.profileContainer}>
+        <Text>Profile</Text>
+      </View>
     </View>
   )
 }
