dec3a1ddec09d891ff2d53b8162154a0df407000
5Prabesh <pbakhrel5@gmail.com>
2024-07-26 17:44:17 +0545

    added doctors list who are available

--- src/Screens/Categories/DoctorScreen.js
+++ src/Screens/Categories/DoctorScreen.js
@@ -1,34 +1,49 @@
-import { View, Text, StyleSheet,Image } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import React from 'react';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import SearchField from '../../Components/SearchField';
+import ProfessionCard from '../../Components/ProfessionCard';
+import professionalsData from '../../assets/data/professionalsdata';
 
-
-
-const DoctorScreen = () => {
+const DoctorScreen = ({navigation}) => {
+    const doctors = professionalsData.filter(item => item.profession === 'Doctor' && item.isAvailable === 'true');
     return (
         <View style={{ backgroundColor: 'white', flex: 1 }}>
             <View style={styles.headerContainer}>
-                <FontAwesome5
-                    name={'chevron-left'}
-                    size={30}
-                    style={styles.backIcon}
-                />
+                <TouchableOpacity onPress={()=>navigation.goBack()}>
+                    <FontAwesome5
+                        name={'chevron-left'}
+                        size={30}
+                        style={styles.backIcon}
+                    />
+                </TouchableOpacity>
                 <View style={styles.headerTextContainer}>
                     <Text style={styles.headerText}>
                         Doctors
                     </Text>
                 </View>
             </View>
+
             <View style={styles.searchContainer}>
-                <SearchField label={'Search'} iconName={'search'}/>
+                <SearchField label={'Search'} iconName={'search'} />
             </View>
-            <View>
-                <View>
-                    <Image source={require('../../assets/images/DoctorImage.png')} style={styles.image}/>
-                </View>
+
+            <View style={{
+                marginTop: 10,
+                marginLeft: 20
+            }}>
+                <Text style={{
+                    color: 'black',
+                    fontWeight: '700',
+                    fontSize: 16
+                }}>
+                    List of Doctors
+                </Text>
             </View>
+            <ProfessionCard
+                professionals={doctors}
+            />
         </View>
     );
 };
@@ -52,13 +67,10 @@ const styles = StyleSheet.create({
         fontWeight: '700',
         textAlign: 'center'
     },
-    searchContainer:{
+    searchContainer: {
         marginTop: 20
     },
-    image:{
-        width: 60, 
-        height:60
-    }
-})
+
+});
 
 export default DoctorScreen;
