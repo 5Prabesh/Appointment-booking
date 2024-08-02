82c8bb51ab2078559516121875ebe409036c5e28
5Prabesh <pbakhrel5@gmail.com>
2024-08-02 12:25:21 +0545

    worked on responsiveness

--- src/Screens/AuthScreens/ForgotPasswordScreen.js
+++ src/Screens/AuthScreens/ForgotPasswordScreen.js
@@ -1,48 +1,52 @@
-import { View, Text, Dimensions, StatusBar, ScrollView, StyleSheet } from 'react-native'
-import React from 'react'
-import ImageContainer from '../../Components/ImageContainer'
-import InputField from '../../Components/InputField'
-import CustomButton from '../../Components/CustomButton'
+import { View, Text, StatusBar, ScrollView, StyleSheet } from 'react-native';
+import React from 'react';
+import ImageContainer from '../../Components/ImageContainer';
+import InputField from '../../Components/InputField';
+import CustomButton from '../../Components/CustomButton';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 const ForgotPasswordScreen = ({ navigation }) => {
-  const { width, height } = Dimensions.get('screen')
   const styles = StyleSheet.create({
     forgotTextContainer: {
-      margin: 10
+      marginVertical: hp(2),
+      marginHorizontal: wp(5),
     },
     forgotText: {
       fontWeight: 'bold',
       color: 'black',
-      fontSize: 30
+      fontSize: wp(6.5)
     },
-    emailContainer:{
-      marginTop: 15,
-      marginLeft: 10,
-    }
-  })
+    emailContainer: {
+      marginTop: hp(0.01),
+      marginHorizontal: wp(5),
+    },
+    buttonContainer: {
+      marginHorizontal: wp(5),
+      marginBottom: hp(2),
+    },
+  });
+
   return (
-    <View style={{flex:1, backgroundColor:'white'}}>
+    <View style={{ flex: 1, backgroundColor: 'white' }}>
       <StatusBar backgroundColor={'white'} barStyle={'dark-content'} />
-      <ImageContainer path={require('../../assets/images/ForgotPasswordImage.png')} imageheight={height * 0.45} onPress={() => navigation.goBack()} />
-      <ScrollView>
-        <View style={styles.forgotTextContainer}>
-          <Text style={styles.forgotText}>
-            Forgot
-          </Text>
-          <Text style={styles.forgotText}>
-            your Password?
-          </Text>
+      <ImageContainer path={require('../../assets/images/ForgotPasswordImage.png')} imageheight={wp(90)} onPress={() => navigation.goBack()} />
+      <ScrollView contentContainerStyle={{ flexGrow: 1, justifyContent: 'space-between' }}>
+        <View>
+          <View style={styles.forgotTextContainer}>
+            <Text style={styles.forgotText}>Forgot</Text>
+            <Text style={styles.forgotText}>your Password?</Text>
+          </View>
+          <View style={styles.forgotTextContainer}>
+            <Text style={{ color: 'grey' }}>
+              Please enter the email address you'd like your password reset information sent to
+            </Text>
+          </View>
+          <View style={styles.emailContainer}>
+            <InputField label={'Enter email address'} />
+          </View>
         </View>
-        <View style={styles.forgotTextContainer}>
-          <Text style={{color : 'grey'}}>
-            Please enter the email address you’d like your password reset information sent to
-          </Text>
-        </View>
-        <View style={styles.emailContainer}>
-          <InputField label={'Enter email address'} />
-        </View>
-        <View style={{marginTop: 25}}>
-        <CustomButton label={'Continue'} onPress={()=>{navigation.navigate('Otp')}}/>
+        <View style={styles.buttonContainer}>
+          <CustomButton label={'Continue'} onPress={() => { navigation.navigate('Otp') }} />
         </View>
       </ScrollView>
     </View>
--- src/Screens/AuthScreens/OtpScreen.js
+++ src/Screens/AuthScreens/OtpScreen.js
@@ -1,108 +1,115 @@
-import { View, Text, Dimensions, StatusBar, ScrollView, StyleSheet, TextInput } from 'react-native'
-import React, {useRef} from 'react'
-import ImageContainer from '../../Components/ImageContainer'
-import CustomButton from '../../Components/CustomButton'
+import { View, Text, Dimensions, StatusBar, ScrollView, StyleSheet, TextInput } from 'react-native';
+import React, { useRef } from 'react';
+import ImageContainer from '../../Components/ImageContainer';
+import CustomButton from '../../Components/CustomButton';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 const OtpScreen = ({ navigation }) => {
-    const { height, width } = Dimensions.get('screen')
-    const styles = StyleSheet.create({
-        otpContainer: {
-            margin: 10,
-        },
-        otpText: {
-            fontWeight: 'bold',
-            color: 'black',
-            fontSize: 30
-        },
-        otpInput : {
-            backgroundColor: 'white',
-            height: 50,
-            width: 50,
-            borderRadius: 15,
-            textAlign:'center',
-            borderWidth:1
-        },
-        otpInputContainerWrap:{
-            marginTop: 10,
-            flexDirection: 'row',
-            justifyContent:'center'
-        },
-        otpInputContainer:{
-            paddingLeft: 15
-        }
-    })
+  const styles = StyleSheet.create({
+    otpContainer: {
+      marginVertical: hp(2),
+      marginHorizontal: wp(5),
+    },
+    otpDescriptionContainer: {
+      marginVertical: hp(0.1),
+      marginHorizontal: wp(5),
+    },
+    otpText: {
+      fontWeight: 'bold',
+      color: 'black',
+      fontSize: wp(6.5),
+    },
+    otpInput: {
+      backgroundColor: 'white',
+      height: hp(7),
+      width: wp(12),
+      borderRadius: 15,
+      textAlign: 'center',
+      borderWidth: 1,
+    },
+    otpInputContainerWrap: {
+      marginTop: hp(2),
+      flexDirection: 'row',
+      justifyContent: 'center',
+    },
+    otpInputContainer: {
+      paddingLeft: wp(2),
+    },
+    buttonContainer: {
+      marginHorizontal: wp(5),
+      marginBottom: hp(2),
+    },
+  });
 
-    const input1Ref = useRef(null);
-    const input2Ref = useRef(null);
-    const input3Ref = useRef(null);
-    const input4Ref = useRef(null);
+  const input1Ref = useRef(null);
+  const input2Ref = useRef(null);
+  const input3Ref = useRef(null);
+  const input4Ref = useRef(null);
 
-    const handleInputChange = (text, ref) => {
-        if(text.length === 1) {
-            ref.current.focus();
-        }
+  const handleInputChange = (text, ref) => {
+    if (text.length === 1) {
+      ref.current.focus();
     }
-    return (
-        <View style={{flex:1, backgroundColor:'white'}}>
-            <StatusBar backgroundColor={'white'} barStyle={'dark-content'} />
-            <ImageContainer path={require('../../assets/images/OtpImage.png')} imageheight={height * 0.4} onPress={() => navigation.goBack()} />
-            <ScrollView>
-                <View style={styles.otpContainer}>
-                    <Text style={styles.otpText}>
-                        Enter OTP
-                    </Text>
-                </View>
-                <View style={styles.otpContainer}>
-                    <Text style={{color:'grey'}}>
-                    Enter 4 digit verification code sent to your registered mobile number.
-                    </Text>
-                </View>
-                <View style={styles.otpInputContainerWrap}>
-                    <View style={styles.otpInputContainer}>
-                        <TextInput 
-                        style={styles.otpInput} 
-                        keyboardType='number-pad' 
-                        maxLength={1} 
-                        ref={input1Ref}
-                        onChangeText={text => handleInputChange(text, input2Ref)}
-                        
-                        />
-                    </View>
-                    <View style={styles.otpInputContainer}>
-                        <TextInput 
-                        style={styles.otpInput} 
-                        keyboardType='number-pad' 
-                        maxLength={1} 
-                        ref={input2Ref}
-                        onChangeText={text => handleInputChange(text, input3Ref)}
-                        
-                        />
-                    </View>
-                    <View style={styles.otpInputContainer}>
-                        <TextInput 
-                        style={styles.otpInput} 
-                        keyboardType='number-pad' 
-                        maxLength={1} 
-                        ref={input3Ref}
-                        onChangeText={text => handleInputChange(text, input4Ref)}
-                        
-                        />
-                    </View>
-                    <View style={styles.otpInputContainer}>
-                        <TextInput 
-                        style={styles.otpInput} 
-                        keyboardType='number-pad' 
-                        maxLength={1} 
-                        
-                        ref={input4Ref}/>
-                    </View>
-                </View>
-                <View style={{marginTop: 80}}>
-                    <CustomButton label={'Submit'} onPress={()=>navigation.navigate('ChangePassword')}/>
-                </View>
-            </ScrollView>
-        </View>
-    )
-}
+  };
 
-export default OtpScreen
+  return (
+    <View style={{ flex: 1, backgroundColor: 'white' }}>
+      <StatusBar backgroundColor={'white'} barStyle={'dark-content'} />
+      <ImageContainer path={require('../../assets/images/OtpImage.png')} imageheight={hp(50)} onPress={() => navigation.goBack()} />
+      <ScrollView contentContainerStyle={{ flexGrow: 1, justifyContent: 'space-between' }}>
+        <View>
+          <View style={styles.otpContainer}>
+            <Text style={styles.otpText}>Enter OTP</Text>
+          </View>
+          <View style={styles.otpDescriptionContainer}>
+            <Text style={{ color: 'grey' }}>
+              Enter the 4-digit verification code sent to your registered mobile number.
+            </Text>
+          </View>
+          <View style={styles.otpInputContainerWrap}>
+            <View style={styles.otpInputContainer}>
+              <TextInput
+                style={styles.otpInput}
+                keyboardType='number-pad'
+                maxLength={1}
+                ref={input1Ref}
+                onChangeText={text => handleInputChange(text, input2Ref)}
+              />
+            </View>
+            <View style={styles.otpInputContainer}>
+              <TextInput
+                style={styles.otpInput}
+                keyboardType='number-pad'
+                maxLength={1}
+                ref={input2Ref}
+                onChangeText={text => handleInputChange(text, input3Ref)}
+              />
+            </View>
+            <View style={styles.otpInputContainer}>
+              <TextInput
+                style={styles.otpInput}
+                keyboardType='number-pad'
+                maxLength={1}
+                ref={input3Ref}
+                onChangeText={text => handleInputChange(text, input4Ref)}
+              />
+            </View>
+            <View style={styles.otpInputContainer}>
+              <TextInput
+                style={styles.otpInput}
+                keyboardType='number-pad'
+                maxLength={1}
+                ref={input4Ref}
+              />
+            </View>
+          </View>
+        </View>
+        <View style={styles.buttonContainer}>
+          <CustomButton label={'Submit'} onPress={() => navigation.navigate('ChangePassword')} />
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+export default OtpScreen;
