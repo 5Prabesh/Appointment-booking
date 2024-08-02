dea27741b55d0b4e086deb5c010e7c3611da9f5a
5Prabesh <pbakhrel5@gmail.com>
2024-08-02 12:46:33 +0545

    worked in responsiveness

--- src/Screens/AuthScreens/ChangePasswordScreen.js
+++ src/Screens/AuthScreens/ChangePasswordScreen.js
@@ -1,50 +1,55 @@
-import { View, Text, StatusBar, Dimensions, ScrollView,StyleSheet } from 'react-native'
-import React from 'react'
-import ImageContainer from '../../Components/ImageContainer'
-import InputField from '../../Components/InputField'
-import CustomButton from '../../Components/CustomButton'
+import { View, Text, StatusBar, Dimensions, ScrollView, StyleSheet } from 'react-native';
+import React from 'react';
+import ImageContainer from '../../Components/ImageContainer';
+import InputField from '../../Components/InputField';
+import CustomButton from '../../Components/CustomButton';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
-const ChangePasswordScreen = ({navigation}) => {
-    const {width,height} = Dimensions.get('screen')
-    const styles = StyleSheet.create({
-        resetPasswordContainer: {
-            margin: 10
-        },
-        resetPasswordText:{
-            color: 'black',
-            fontSize: 25,
-            fontWeight: 'bold', 
-        },
-        changePasswordContainer:{
-            marginLeft: 10
-        },
-        button:{
-            marginTop: 35
-        }
-    })
+const ChangePasswordScreen = ({ navigation }) => {
+  const styles = StyleSheet.create({
+    resetPasswordContainer: {
+      marginVertical: hp(2),
+      marginHorizontal: wp(5),
+    },
+    resetPasswordText: {
+      color: 'black',
+      fontSize: wp(6),
+      fontWeight: 'bold',
+    },
+    changePasswordContainer: {
+      marginHorizontal: wp(5),
+    },
+    buttonContainer: {
+      marginHorizontal: wp(5),
+      marginBottom: hp(2),
+    },
+  });
+
   return (
-    <View style={{flex:1, backgroundColor:'white'}}>
-      <StatusBar backgroundColor={'white'} barStyle={'dark-content'}/>
-      <ImageContainer path={require('../../assets/images/ChangePasswordImage.png')} imageheight={height*0.46} onPress={()=>navigation.goBack()}/>
-      <ScrollView>
-        <View style={styles.resetPasswordContainer}>
+    <View style={{ flex: 1, backgroundColor: 'white' }}>
+      <StatusBar backgroundColor={'white'} barStyle={'dark-content'} />
+      <ImageContainer path={require('../../assets/images/ChangePasswordImage.png')} imageheight={hp(46)} onPress={() => navigation.goBack()} />
+      <ScrollView contentContainerStyle={{ flexGrow: 1, justifyContent: 'space-between' }}>
+        <View>
+          <View style={styles.resetPasswordContainer}>
             <Text style={styles.resetPasswordText}>
-                Reset 
+              Reset
             </Text>
             <Text style={styles.resetPasswordText}>
-                Password?
+              Password?
             </Text>
+          </View>
+          <View style={styles.changePasswordContainer}>
+            <InputField label={'Enter new password'} type={'password'} />
+            <InputField label={'Confirm new password'} type={'password'} />
+          </View>
         </View>
-        <View style={styles.changePasswordContainer}>
-            <InputField label={'Enter new password'} type={'password'}/>
-            <InputField label={'Confirm new password'} type={'password'}/>
-        </View>
-        <View style={styles.button}>
-            <CustomButton label={'Confirm'} onPress={()=>navigation.navigate('Login')}/>
+        <View style={styles.buttonContainer}>
+          <CustomButton label={'Confirm'} onPress={() => navigation.navigate('Login')} />
         </View>
       </ScrollView>
     </View>
-  )
-}
+  );
+};
 
-export default ChangePasswordScreen
+export default ChangePasswordScreen;
