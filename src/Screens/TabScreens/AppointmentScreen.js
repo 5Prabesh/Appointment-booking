2314ef71067b949199621465c5cf9f832ddd85f5
5Prabesh <pbakhrel5@gmail.com>
2024-08-11 14:08:38 +0545

    appointment screen completed and made responsive

--- src/Screens/TabScreens/AppointmentScreen.js
+++ src/Screens/TabScreens/AppointmentScreen.js
@@ -1,7 +1,9 @@
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import React from 'react';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import AntDesign from 'react-native-vector-icons/AntDesign'
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import CustomButton from '../../Components/CustomButton';
 
 const AppointmentScreen = ({ route, navigation }) => {
     const { profession } = route.params;
@@ -15,14 +17,37 @@ const AppointmentScreen = ({ route, navigation }) => {
                         style={styles.backIcon}
                     />
                 </TouchableOpacity>
+
                 <View style={styles.headerTextContainer}>
-                    <Text style={styles.headerText}>Appointment</Text>
+                    <Text style={styles.headerText}>
+                        Appointment
+                    </Text>
                 </View>
             </View>
-            <View style={styles.contentContainer}>
+            <View style={styles.profileImageContainer}>
+                <Image
+                    source={{ uri: profession.image }}
+                    style={styles.profileImage}
+                />
+            </View>
+            <View style={styles.informationContainer}>
                 <Text style={styles.name}>{profession.name}</Text>
                 <Text style={styles.qualification}>{profession.qualification}</Text>
                 <Text style={styles.description}>{profession.description}</Text>
+            </View>
+            <View style={styles.contactMethodContainer}>
+                <TouchableOpacity activeOpacity={0.3}>
+                    <AntDesign name={'phone'} size={22} color={'#001A23'} />
+                </TouchableOpacity>
+                <TouchableOpacity activeOpacity={0.3}>
+                    <AntDesign name={'videocamera'} size={22} color={'#001A23'} />
+                </TouchableOpacity>
+                <TouchableOpacity activeOpacity={0.3}>
+                    <AntDesign name={'message1'} size={22} color={'#001A23'} />
+                </TouchableOpacity>
+            </View>
+            <View style={styles.bookButton}>
+                <CustomButton label={'Book an Appointment'}/>
             </View>
         </View>
     );
@@ -30,9 +55,9 @@ const AppointmentScreen = ({ route, navigation }) => {
 
 const styles = StyleSheet.create({
     headerContainer: {
-        flexDirection: 'row',
         marginTop: 12,
         marginLeft: 12,
+        flexDirection: 'row',
     },
     backIcon: {
         color: 'black',
@@ -45,23 +70,50 @@ const styles = StyleSheet.create({
         color: 'black',
         fontSize: wp(6),
         fontWeight: '700',
-        textAlign: 'center',
+        textAlign: 'center'
     },
-    contentContainer: {
-        padding: 20,
+    profileImage: {
+        height: hp(30),
+        width: wp(55),
+        borderRadius: wp(50),
+    },
+    profileImageContainer: {
+        margin: 20,
+        alignItems: 'center',
+    },
+    informationContainer: {
+        margin: 10,
+        alignItems: 'center'
     },
     name: {
-        fontSize: wp(5),
-        fontWeight: 'bold',
-        marginBottom: 10,
+        fontSize: wp(6),
+        fontWeight: '700',
+        color: 'black',
     },
     qualification: {
-        fontSize: wp(4),
-        marginBottom: 10,
+        fontWeight: '600',
+        color: 'black',
     },
     description: {
-        fontSize: wp(3.5),
+        fontSize: wp(4),
+        textAlign: 'justify',
+        marginLeft: 40,
+        marginRight: 40
     },
+    contactMethodContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginTop: 20,
+    },
+    bookButton: {
+        position: 'absolute',
+        bottom: hp(1),
+        right:0,
+        left:0,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
 });
 
 export default AppointmentScreen;
